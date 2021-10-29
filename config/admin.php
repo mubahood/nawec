00df6634e75a334fd5da495391c8c9899d441fb8
Muhindo Mubaraka <mubs0x@gmail.com>
2021-10-30 01:56:20 +0300

    new data

--- config/admin.php
+++ config/admin.php
@@ -11,7 +11,7 @@ return [
     | login page.
     |
     */
-    'name' => 'Laravel-admin',
+    'name' => 'NATIONAL WATER AND ELECTRICITY COMPANY (NAWEC)',
 
     /*
     |--------------------------------------------------------------------------
